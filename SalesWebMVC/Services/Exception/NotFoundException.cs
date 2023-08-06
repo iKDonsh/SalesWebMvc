4cca83349b3a1403ebe84cafedbd8384970b6b66
Vinicius Lopes <ikdonsh@hotmail.com>
2023-08-06 18:14:14 -0300

    Update seller and custom service exception

---
+++ SalesWebMVC/Services/Exception/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMVC.Services.Exception
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
