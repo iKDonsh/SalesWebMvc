ba777db19610b846d2e78c765490e440d3488f27
Vinicius Lopes <ikdonsh@hotmail.com>
2023-08-07 17:32:13 -0300

    Exception handling for delete (referential integrity)

---
+++ SalesWebMVC/Services/Exception/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMVC.Services.Exception
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
